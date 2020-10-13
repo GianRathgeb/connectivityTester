0868a084d019ff0e998ca19c6bc05ea79f48fbd2
Rag <gian.rathgeb@brel.ch>
2020-10-13 14:09:44 +0200

    changed to another format

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def fnTestConnectivity(siteAddress):
     objHost = ping(address=siteAddress, count=1)
 
     if objHost.is_alive:
-        return "Address {}, ({}), can be reached".format(objHost.address, siteAddress)
+        return "Address {}, ({}), can be reached".format(siteAddress, objHost.address)
     else:
         return "No connection to {}".format(siteAddress)
 
