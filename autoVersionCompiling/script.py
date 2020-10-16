6d8780d3fec43fa7d4de73292935a5b57ef67c0a
GianRathgeb <gian.rathgeb@bbz-sh.ch>
2020-10-16 12:32:21 +0200

    Better useroutput output and compiling

--- autoVersionCompiling/script.py
+++ autoVersionCompiling/script.py
@@ -3,6 +3,10 @@ import PyInstaller.__main__
 import os
 from packaging.version import Version
 import shutil
+# Output with color
+from colorama import Fore, init
+# After print reset color
+init(autoreset=True)
 
 # Import writeVersionfile.py
 import writeVersionFile as vf
@@ -42,47 +46,50 @@ if not os.path.isfile(strBuildFilePath):
 # Read the last compiled version from the file
 with open(strBuildFilePath, "r") as buildInfo:
     verLastVersion = Version(buildInfo.read())
+
 # Define the new version and set it equal to last verison
-with open(strBuildFilePath, "w") as buildInfo:
-    arrNewVersion = [
-        verLastVersion.release[0],
-        verLastVersion.release[1],
-        verLastVersion.release[2],
-        verLastVersion.release[3],
-    ]
-    # Update the patch version in new version
-    arrNewVersion[2] += 1
-    # Build string with new version
-    strNewVersion = ""
-    for index in arrNewVersion:
-        strNewVersion += str(index) + "."
-    # Everything but not last char (is . and don't work with version)
-    strNewVersion = strNewVersion[0:-1]
-    # Write new version to build file
-    buildInfo.write(strNewVersion)
+arrNewVersion = [
+    verLastVersion.release[0],
+    verLastVersion.release[1],
+    verLastVersion.release[2],
+    verLastVersion.release[3],
+]
+# Update the patch level in new version
+arrNewVersion[2] += 1
+# Build string with new version
+strNewVersion = ""
+for index in arrNewVersion:
+    strNewVersion += str(index) + "."
+# Everything but not last char (is . and don't work with version)
+strNewVersion = strNewVersion[0:-1]
+
+
 # Create the output path for the compiled file
 strCompileDirPath = str(f"{strInputPath[0:-3]}-{strNewVersion}")
 # Try to create this directory
 try:
     os.mkdir(strCompileDirPath)
 except OSError:
-    print("Error while Compiling")
+    print(f"{Fore.RED}Error while Compiling")
     exit
 else:
     # Copy script to compiled folder
-    print(f"Created folder for compiled files: {strCompileDirPath}")
+    print(f"{Fore.BLUE}Created folder for compiled files: {strCompileDirPath}")
     shutil.copy(f"./{strInputPath}", f"{strCompileDirPath}")
-    print(f"Copy {strInputPath} to {strCompileDirPath}")
+    print(f"{Fore.BLUE}Copy {strInputPath} to {strCompileDirPath}")
 
     # Rename the copied script (oldname-version)
-    inputFile = str(f"{os.path.join(strCompileDirPath, strInputPath)}")
-    outputFile = str(f"{strInputPath[0:-3]}-{strNewVersion}.py")
-    outputPath = str(f"{os.path.join(strCompileDirPath, outputFile)}")
-    os.rename(inputFile, outputPath)
-    print(f"Renamed output file to {outputFile}")
+    strInputFile = str(f"{os.path.join(strCompileDirPath, strInputPath)}")
+    strOutputFile = str(f"{strInputPath[0:-3]}-{strNewVersion}.py")
+    strOutputPath = str(f"{os.path.join(strCompileDirPath, strOutputFile)}")
+    os.rename(strInputFile, strOutputPath)
+    print(f"{Fore.BLUE}Renamed output file to {strOutputFile}")
 
     # Write version file
-    vf.writeVersionFile(strVersionFile, tuple(arrNewVersion), strAuthor, strProjectName, strNewVersion)
+    vf.writeVersionFile(strVersionFile, tuple(arrNewVersion),
+                        strAuthor, strProjectName, strNewVersion)
+
+    print(f"{Fore.BLUE}Begin compiling {strInputPath}")
     # Run Pyinstaller and compile script
     PyInstaller.__main__.run([
         '--version-file=../%s' % strVersionFile,
@@ -93,11 +100,18 @@ else:
         strCompileDirPath,
         '--workpath',
         os.path.join(strCompileDirPath, "compile"),
-        outputPath,
+        strOutputPath,
     ])
     # Remove unnecessary files and dirs (temporary generated files)
-    strSpecFile = f"{outputFile[0:-3]}.spec"
+    strSpecFile = f"{strOutputFile[0:-3]}.spec"
     os.remove(f"{os.path.join(strCompileDirPath, strSpecFile)}")
     shutil.rmtree(os.path.join(strCompileDirPath, "compile"))
 
-    print(f"Compiled script {strInputPath} to {outputPath}")
+    print(f"{Fore.BLUE}Compiled script {strInputPath} to {strOutputPath}")
+
+    # Open buildfile with write rights and write new version to file
+    with open(strBuildFilePath, "w") as buildInfo:
+        # Write new version to build file
+        buildInfo.write(strNewVersion)
+
+    print(f"{Fore.BLUE}Write new version ({strNewVersion}) to file")
--- autoVersionCompiling/writeVersionFile.py
+++ autoVersionCompiling/writeVersionFile.py
@@ -1,33 +1,41 @@
 import os
+from colorama import init, Fore
+init(autoreset=True)
+
 
 def writeVersionFile(strVersionFile, tupleVersion, strAuthor, strName, strVersion):
-    with open(strVersionFile, 'w') as vf:
-        vf.write(f"""VSVersionInfo(
-            ffi=FixedFileInfo(
-                filevers={tupleVersion},
-                prodvers={tupleVersion},
-                mask=0x3f,
-                flags=0x0,
-                OS=0x4,
-                fileType=0x1,
-                subtype=0x0,
-                date=(0, 0)
-            ),
-            kids=[
-                StringFileInfo(
-                    [
-                        StringTable(
-                            u'040904B0',
-                            [StringStruct(u'CompanyName', u'{strAuthor}'),
-                             StringStruct(u'FileDescription', u'{strName}'),
-                             StringStruct(u'FileVersion', u'{tupleVersion}'),
-                             StringStruct(u'InternalName', u'{strName.upper()}'),
-                             StringStruct(u'LegalCopyright',
-                                          u'Copyright (c) {strAuthor}'),
-                             StringStruct(u'OriginalFilename', u'{strName}.exe'),
-                             StringStruct(u'ProductName', u'{strName}'),
-                             StringStruct(u'ProductVersion', u'{strVersion}')])
-                    ]),
-                VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
-            ]
-        )""")
+    print(f"{Fore.BLUE}Write Verison file")
+    try:
+        with open(strVersionFile, 'w') as vf:
+            vf.write(f"""VSVersionInfo(
+                ffi=FixedFileInfo(
+                    filevers={tupleVersion},
+                    prodvers={tupleVersion},
+                    mask=0x3f,
+                    flags=0x0,
+                    OS=0x4,
+                    fileType=0x1,
+                    subtype=0x0,
+                    date=(0, 0)
+                ),
+                kids=[
+                    StringFileInfo(
+                        [
+                            StringTable(
+                                u'040904B0',
+                                [StringStruct(u'CompanyName', u'{strAuthor}'),
+                                StringStruct(u'FileDescription', u'{strName}'),
+                                StringStruct(u'FileVersion', u'{tupleVersion}'),
+                                StringStruct(u'InternalName', u'{strName.upper()}'),
+                                StringStruct(u'LegalCopyright',
+                                            u'Copyright (c) {strAuthor}'),
+                                StringStruct(u'OriginalFilename', u'{strName}.exe'),
+                                StringStruct(u'ProductName', u'{strName}'),
+                                StringStruct(u'ProductVersion', u'{strVersion}')])
+                        ]),
+                    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+                ]
+            )""")
+        print(f"{Fore.BLUE}Writing {strVersionFile} Done")
+    except:
+        print(f"{Fore.RED}Failed writing {strVersionFile}")
