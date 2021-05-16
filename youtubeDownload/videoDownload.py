2c3a2aed617cb702981a676e31ef9c5fe1f180f2
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2021-05-16 14:17:40 +0200

    updated the video downloader

--- youtubeDownload/videoDownload.py
+++ youtubeDownload/videoDownload.py
@@ -2,23 +2,36 @@
 import pafy
 
 # Full video url and new video object
-url = "https://www.youtube.com/watch?v=kQoxbDOWjf4"
-video = pafy.new(url)
+# https://www.youtube.com/watch?v=kQoxbDOWjf4
+def download():
+    url = input("Enter url from video: ")
 
-# Get all aviable video streams
-streams = video.streams
+    video = pafy.new(url)
 
-print("Quality options:")
-# Print every aviable video stream
-for i in streams:
-    print(i)
+    # Get all aviable video streams
+    streams = video.allstreams
 
-# Get the best resolution
-bestRest = video.getbest()
-# Print the best resolution
-print(bestRest.resolution, bestRest.extension)
+    print("Quality options:")
+    # Print every aviable video stream
+    for i, e in enumerate(streams):
+        print(f"Stream {i}: {str(e).split('@')[1]}")
+    choice = input("Choose video stream: ")
+    # Get the best resolution
+    #res = video.getbest()
 
-# Downlaod the video
-bestRest.download()
-# Print successfull message
-print("Video downloaded successfully")
+    res = streams[int(choice)]
+    # Print the best resolution
+    print(res.resolution, res.extension)
+
+    # Downlaod the video
+    res.download()
+    # Print successfull message
+    print("Video downloaded successfully")
+
+    t = input("Do you want to download another video? y/n [n] ")
+    if t == "y" or t == "Y":
+        download()
+    else: exit()
+
+
+download()
